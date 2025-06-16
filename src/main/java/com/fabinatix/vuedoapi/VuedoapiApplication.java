d54f28c4364b0da389bc7e7abdf4518e00b8d761
Fabinatix97 <fabian@fabi-online.de>
2025-06-16 20:51:30 +0200

    Initial commit

---
+++ src/main/java/com/fabinatix/vuedoapi/VuedoapiApplication.java
@@ -0,0 +1,13 @@
+package com.fabinatix.vuedoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuedoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuedoapiApplication.class, args);
+	}
+
+}
