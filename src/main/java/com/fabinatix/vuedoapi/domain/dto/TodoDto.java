ec02b364c1f4952ddb40d8ba23afd2d733b83dd3
Fabinatix97 <fabian@fabi-online.de>
2025-06-17 21:19:01 +0200

    Add DTOs and mappers

---
+++ src/main/java/com/fabinatix/vuedoapi/domain/dto/TodoDto.java
@@ -0,0 +1,31 @@
+package com.fabinatix.vuedoapi.domain.dto;
+
+import java.util.UUID;
+
+import com.fabinatix.vuedoapi.domain.entities.TodoPriority;
+import com.fabinatix.vuedoapi.domain.entities.TodoStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TodoDto {
+
+  private UUID id;
+
+  private String title;
+
+  private String description;
+
+  private TodoStatus status;
+
+  private TodoPriority priority;
+
+  private TodoListDto todoList;
+
+}
---
+++ src/main/java/com/fabinatix/vuedoapi/mappers/impl/TodoMapperImpl.java
@@ -0,0 +1,28 @@
+package com.fabinatix.vuedoapi.mappers.impl;
+
+import org.modelmapper.ModelMapper;
+
+import com.fabinatix.vuedoapi.domain.dto.TodoDto;
+import com.fabinatix.vuedoapi.domain.entities.Todo;
+import com.fabinatix.vuedoapi.mappers.Mapper;
+
+public class TodoMapperImpl implements Mapper<Todo, TodoDto> {
+
+  private ModelMapper modelMapper;
+  
+
+  public TodoMapperImpl(ModelMapper modelMapper) {
+    this.modelMapper = modelMapper;
+  }
+
+  @Override
+  public TodoDto mapTo(Todo todo) {
+    return modelMapper.map(todo, TodoDto.class);
+  }
+
+  @Override
+  public Todo mapFrom(TodoDto todoDto) {
+    return modelMapper.map(todoDto, Todo.class);
+  }
+
+}
