56891487eab2a36df456a4b7f4fed10e15eb9dbd
Fabinatix97 <fabian@fabi-online.de>
2025-06-19 17:41:45 +0200

    Implement Todo and TodoList controllers, services, and their respective service implementations

--- src/main/java/com/fabinatix/vuedoapi/services/TodoListService.java
+++ src/main/java/com/fabinatix/vuedoapi/services/TodoListService.java
@@ -1,5 +1,21 @@
 package com.fabinatix.vuedoapi.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.fabinatix.vuedoapi.domain.entities.TodoList;
+
 public interface TodoListService {
+
+  TodoList save(TodoList todoList);
+
+  List<TodoList> findAll();
+
+  Optional<TodoList> findOne(UUID todoListId);
+
+  TodoList update(UUID todoListId, TodoList todoList);
+  
+  void delete(UUID todoListId);
 
 }
--- src/main/java/com/fabinatix/vuedoapi/services/TodoService.java
+++ src/main/java/com/fabinatix/vuedoapi/services/TodoService.java
@@ -1,5 +1,21 @@
 package com.fabinatix.vuedoapi.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.fabinatix.vuedoapi.domain.entities.Todo;
+
 public interface TodoService {
+
+  Todo save(UUID todoListId, Todo todo);
+
+  List<Todo> findAll(UUID todoListId);
+
+  Optional<Todo> findOne(UUID todo_list_id, UUID todo_id);
+
+  Todo update(UUID todoListId, UUID todoId, Todo todo);
+
+  void delete(UUID todo_list_id, UUID todo_id);
 
 }
