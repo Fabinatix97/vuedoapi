aae77b9d0f5eb746ce203aaefc4355b5dd79f457
Fabinatix97 <fabian@fabi-online.de>
2025-06-21 12:08:34 +0200

    Add fetch type to todos relationship in TodoList entity

--- src/main/java/com/fabinatix/vuedoapi/domain/entities/TodoList.java
+++ src/main/java/com/fabinatix/vuedoapi/domain/entities/TodoList.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +36,7 @@ public class TodoList {
   @Column(name = "description")
   private String description;
 
-  @OneToMany(mappedBy = "todoList", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "todoList", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
   private List<Todo> todos;
 
 }
